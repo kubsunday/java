d71f23cb22b904cd68ed7bb6267a7c09815ab222
kubsunday <115043642+kubsunday@users.noreply.github.com>
2022-11-23 17:28:33 +0100

    Scanner to github

--- Scanner.java
+++ Scanner.java
@@ -1,5 +1,10 @@
 public class Scanner {
     public static void main(String[] args) {
+        int x, y, z;
+            x = 1;
+            y = 2;
+            z = x + y;
+        System.out.println(y);
         
     }
 }
